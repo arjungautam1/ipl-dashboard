9695866e46bf27a40755b1c87a68139e8d772ebb
arjun-sudo <arjungautam5431@gmail.com>
2021-04-26 20:42:52 +0545

    first commit

---
+++ src/main/java/me/arjungautam/ipldashboard/IpldashboardApplication.java
@@ -0,0 +1,13 @@
+package me.arjungautam.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpldashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/arjungautam/ipldashboard/IpldashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.arjungautam.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
