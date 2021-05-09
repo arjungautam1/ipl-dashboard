e94cf051db3cea45f1174ecb546e0aa88e2ec2fa
arjun-sudo <arjungautam5431@gmail.com>
2021-05-09 14:59:22 +0545

    completed ui for match page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,14 @@
+import {React} from 'react'
+import {MatchSmallCard} from "../components/MatchSmallCard";
+import {MatchDetailCard} from "../components/MatchDetailCard";
+
+export const MatchPage = () => {
+
+
+    return (
+        <div className="MatchPage">
+<h1>Match Page</h1>
+        </div>
+    );
+}
+
